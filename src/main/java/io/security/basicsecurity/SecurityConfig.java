0a1f18dd37addcdf1cac676eb06311a80fd28ad7
Wooksang Ryu <today8934@gmail.com>
2022-07-14 00:29:31 +0900

    SessionManagement 동시 세션 제어 세션 고정 보호 세션 정책

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -80,5 +81,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 tokenValiditySeconds(3600).
                 alwaysRemember(false).
                 userDetailsService(userDetailsService);
+
+        http.sessionManagement().
+                maximumSessions(1).
+                maxSessionsPreventsLogin(false).
+                expiredUrl("/expired").
+                and().
+                sessionFixation().
+                changeSessionId().
+                sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+
     }
 }
